82782078de2486d75ce372c3d0a3662e13d09164
Shahab Lavasani <shahablavasani@outlook.com>
2026-05-06 19:28:41 -0500

    chore: release twoman 1.0.3

--- android-client/app/build.gradle.kts
+++ android-client/app/build.gradle.kts
@@ -110,8 +110,8 @@ android {
         applicationId = "com.twoman.android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.0.2"
+        versionCode = 16
+        versionName = "1.0.3"
         buildConfigField("String", "RUNTIME_LOG_TAG", quoteForGradle(androidLogTag))
         buildConfigField("String", "VPN_SESSION_NAME", quoteForGradle(androidVpnSessionName))
         resValue("string", "runtime_app_name", quoteForGradle(androidAppLabel))
